7034bfd5a18268be420b6eeb97b3fc554d9548de
JohnRFK <juan0924.rf@gmail.com>
2024-04-15 14:38:59 -0600

    errores de persistencia User corregidos

--- src/main/java/com/john/login3/login3/models/User.java
+++ src/main/java/com/john/login3/login3/models/User.java
@@ -7,8 +7,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -25,7 +29,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "users_jwt", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
 public class User implements UserDetails {
     @Id
+    @GeneratedValue
     Integer id;
+    @Basic
     @Column(nullable = false)
     String username;
     @Column(nullable = false)
@@ -36,6 +42,7 @@ public class User implements UserDetails {
     @Column(name = "last_name")
     String lastName;
     String country;
+    @Enumerated(EnumType.STRING)
     Role role;
 
     @Override
