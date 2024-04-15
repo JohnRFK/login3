5f21fbc7ad057bb705b2238fdedbe09c62d689e3
JohnRFK <juan0924.rf@gmail.com>
2024-04-15 13:49:36 -0600

    Se corrigieron warnings

--- src/main/java/com/john/login3/login3/jwt/JwtService.java
+++ src/main/java/com/john/login3/login3/jwt/JwtService.java
@@ -1,13 +1,11 @@
 package com.john.login3.login3.jwt;
 
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -15,7 +13,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureAlgorithm;
 
 
 public class JwtService {
